af029733a799cd2c92b1560cbe83574a69fdc3fd
pal-1 <santhosh.kumarva@dell.com>
2019-04-19 15:23:29 +0530

    finally done

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,19 +1,109 @@
 package org.superbiz.moviefun;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
 
-@SpringBootApplication
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
+
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
+
+    @Bean
+    DatabaseServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapSerivces) {
+        return new DatabaseServiceCredentials(vcapSerivces);
+    }
+
+    @Bean
+    public DataSource albumsDataSource(DatabaseServiceCredentials serviceCredentials) {
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(serviceCredentials.jdbcUrl("albums-mysql"));
+        return dataSource;
+    }
+
+    @Bean
+    public DataSource movieDataSource(DatabaseServiceCredentials serviceCredentials) {
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(serviceCredentials.jdbcUrl("movies-mysql"));
+        return dataSource;
+    }
+
+    @Bean
+    public HibernateJpaVendorAdapter jpaVendorAdapter(){
+
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+        jpaVendorAdapter.setGenerateDdl(true);
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean albumContainerEntityManagerFactoryBean(DataSource albumsDataSource,
+                                                                                         HibernateJpaVendorAdapter albumHibernateJpaVendorAdapter){
+
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(albumsDataSource);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(albumHibernateJpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPackagesToScan(this.getClass().getPackage().getName());
+        localContainerEntityManagerFactoryBean.setPersistenceUnitName("albums");
+        return localContainerEntityManagerFactoryBean;
+
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean moviesContainerEntityManagerFactoryBean(DataSource movieDataSource, HibernateJpaVendorAdapter movieHibernateJpaVendorAdapter){
+
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(movieDataSource);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(movieHibernateJpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPackagesToScan(this.getClass().getPackage().getName());
+        localContainerEntityManagerFactoryBean.setPersistenceUnitName("movies");
+        return localContainerEntityManagerFactoryBean;
+
+    }
+
+    @Bean
+    public PlatformTransactionManager albumsTransactionManager(EntityManagerFactory albumContainerEntityManagerFactoryBean) {
+        return new JpaTransactionManager(albumContainerEntityManagerFactoryBean);
+    }
+
+
+    @Bean
+    public PlatformTransactionManager moviesTransactionManager(EntityManagerFactory moviesContainerEntityManagerFactoryBean) {
+        return new JpaTransactionManager(moviesContainerEntityManagerFactoryBean);
+    }
+
 }
