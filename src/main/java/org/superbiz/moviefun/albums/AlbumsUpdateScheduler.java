2a92ef4e8b4d7e31e24f1803dcb80a6cc47d6330
pal-1 <santhosh.kumarva@dell.com>
2019-04-22 16:22:14 +0530

    background jobs

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -3,9 +3,15 @@ package org.superbiz.moviefun.albums;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Configuration
 @EnableAsync
@@ -18,8 +24,11 @@ public class AlbumsUpdateScheduler {
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    private final JdbcTemplate jdbcTemplate;
+
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, DataSource dataSource) {
         this.albumsUpdater = albumsUpdater;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
@@ -27,12 +36,32 @@ public class AlbumsUpdateScheduler {
     public void run() {
         try {
             logger.debug("Starting albums update");
-            albumsUpdater.update();
+
+            if (startAlbumSchedulerTask()) {
+                logger.debug("Starting albums update");
+                albumsUpdater.update();
+                logger.debug("Finished albums update");
+
+            } else {
+                logger.debug("Nothing to start");
+            }
 
             logger.debug("Finished albums update");
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
+    }
+
+    private boolean startAlbumSchedulerTask() {
+        int updatedRows = jdbcTemplate.update(
+                "UPDATE album_scheduler_task" +
+                        " SET started_at = now()" +
+                        " WHERE started_at IS NULL" +
+                        " OR started_at < date_sub(now(), INTERVAL 2 MINUTE)"
+        );
+
+        return updatedRows > 0;
+
     }
 }
